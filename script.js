bf4752069206114cb7615f8634c179bf550f4168
parijat-datta <parijatdutt@gmail.com>
2020-07-29 18:39:23 +0600

    Finally The Project is Complete with Additional CSS

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Generating 4 digit Random Numbers where Minimum is 1000 and Max is 9999
+
 function randomDigit(){
      const min=1000;
      const max=9999;
@@ -14,23 +16,22 @@ function randomDigit(){
     
     }
     
-    
 
+    //Taking User Input (0-9) and Showing it to the Input Screen
     function userInput(clicked_id){
-
-
-//         let x=clicked_id;
-        let y=clicked_id;
+        let key=clicked_id;
         const str=document.getElementById('numberInput').value;
+        //Only 4-Digit Number will be Accepted
         if(str.length<4){
 
-document.getElementById('numberInput').value+=y;}
-
-
-
+document.getElementById('numberInput').value+=key;}
+        //Alert User if there is already 4 digits Have been Inserted
+        else if(str.length>=4){
+            alert("Pin can contain 4 Digits Only!");
+        }
     }
 
-
+   //Deleting the Last Element of the String using Back Cursor for Bonus Mark
     function backCursor(){
     const str=document.getElementById('numberInput').value;
     
@@ -41,77 +42,83 @@ document.getElementById('numberInput').value+=y;}
     }
 
 
+    //For clearing the Screen when C is Pressed for Bonus Mark
+
     function clearInput(){
      
         const str="";
         document.getElementById('numberInput').value=str;
 
-
-
-
-    }
+}
  
 
+  //Submit Button for the Result
 
     function submitButton(){
 
    const oldStr=document.getElementById('randomPin').value;
    const newStr=document.getElementById('numberInput').value;
 
-   if(oldStr==newStr&oldStr.length!=0&newStr.length!=0){
 
+//Checking if both input is not Null and They are Matched
+   if(oldStr==newStr&oldStr.length!=0&newStr.length!=0){
     document.getElementById('success').style.display="block";
+    document.getElementById('wrong').style.display="none";
+
+    //Everytime The Inputs are Matched The number of chance will be set to 3
+    document.getElementById('chance').innerText='3';
    }
 
    if(oldStr.length==0||newStr.length==0){
-       
+
+    /*Checking if Random number has been Generated and Telling user to Generate
+    the Random Number First*/
      if(oldStr.length==0){
         document.getElementById('randomPin').value="Generate Random Pin First";
+    
+        //if user has not generated the Random Pin the Page will Reload automatically after 5s
         timeFunction(); 
     }
+     
+    /*Checking if user has entered the Input and Telling user to Insert the Pin*/
      if(newStr.length==0){
         document.getElementById('numberInput').value="Enter Your Pin";
      }
+     
+     //if user has not entered the Pin the Page will Reload automatically after 5s
       timeFunction();
    }
 
    if(newStr.length<4){
        
-    
+    /*If user has not inserted 4 digits it will alert user but the number of 
+    chance will not be decreased and the page will reload after 5s*/
        document.getElementById('numberInput').value="Pin Must Contain 4 Numbers";
        timeFunction();
 
   }
 
    else if(oldStr!=newStr&oldStr.length!=0&newStr.length!=0){
-
+    /*The Inputs are not Matched and The strings are not empty*/
 
     document.getElementById('wrong').style.display="block";
-    // let chancesLeft=document.getElementById('chance').innerText;
-    // let chancesLeftNumber=parseInt(chancesLeft);
-    // chancesLeftNumber=chancesLeftNumber-1;
+    document.getElementById('success').style.display="none";
+    /*The Number of Chance will be decreased */
     chanceCount();
    }
-
-
-
-
-
-
 
     }
 
 
 
-
+    //The Page will Reload if User do not Follow the instruction
     function timeFunction() {
         setTimeout(function(){ location.reload();}, 5000);
     }
 
     
-
+    //Counting the Number of Chances left for entering the Pin For Bonus Mark
     function chanceCount(){
-
         const val=document.getElementById('chance').innerText;
         const newVal=parseInt(val);
         let decreaseVal=newVal;
@@ -124,8 +131,10 @@ document.getElementById('numberInput').value+=y;}
         if(decreaseVal==0){
            
           const button=document.getElementById('submit');
+          const calculatorBody=document.getElementById('calBody');
           button.style.background = 'red';
-        button.disabled = true;
+          button.disabled = true;
+        
 
         }
 
